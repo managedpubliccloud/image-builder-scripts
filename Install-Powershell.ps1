f9c7fd01d05b3732dc8dbf2e56abd318ac34e4a0
Nick Scott-Perry <nick.scott-perry@ethan.com.au>
2024-11-24 10:55:55 +1100

    Update from AVD-Pool1-IMG scripts at 2024-11-24 10:55:55

--- Install-Powershell.ps1
+++ Install-Powershell.ps1
@@ -15,45 +15,42 @@
 $AppPublisher = "Microsoft" 
 $AppName = "Powershell"
 $AppVersion = "7.4.6.0"
-#$AppSetupFile = ""
+$AppSetupFile = "PowerShell-7.4.6-win-x64.msi"
 
 $RootFolder = "$env:programdata\ETHAN\ImageBuild"
 $RootFolderApp = Join-Path -Path $RootFolder -ChildPath "$AppPublisher.$AppName"
 $AppMoniker = "$AppPublisher.$AppName.$AppVersion"
 
 # Downloads
-# $AppURI = ""
-# $AppDownloadFile = ""
+$AppURI = "https://github.com/PowerShell/PowerShell/releases/download/v7.4.6/PowerShell-7.4.6-win-x64.msi"
+$AppDownloadFile = "$AppSetupFile"
 
 Write-Host "###### Starting $AppPublisher $AppName ($AppVersion) installation script ######"
 
-
 # Find WinGet
-$WinGet = Get-ChildItem -Path (Join-Path -Path (Join-Path -Path $env:ProgramFiles -ChildPath "WindowsApps") -ChildPath "Microsoft.DesktopAppInstaller*_x64*\Winget.exe")
-Write-Host "WinGet path $Winget"
-
-
+# $WinGet = Get-ChildItem -Path (Join-Path -Path (Join-Path -Path $env:ProgramFiles -ChildPath "WindowsApps") -ChildPath "Microsoft.DesktopAppInstaller*_x64*\Winget.exe")
+# Write-Host "WinGet path $Winget"
 
 
 # Root folder for app
-# if ((Test-Path $RootFolderApp) -eq $false) {
-#     New-Item -Path $RootFolderApp -ItemType Directory -Force
-#     Write-Host "Created $RootFolderApp directory"
-# } else {
-#     Write-Host "$RootFolderApp directory already exists"
-# }
+if ((Test-Path $RootFolderApp) -eq $false) {
+    New-Item -Path $RootFolderApp -ItemType Directory -Force
+    Write-Host "Created $RootFolderApp directory"
+} else {
+    Write-Host "$RootFolderApp directory already exists"
+}
 
 # Download content
-# Write-Host "Downloading $AppMoniker from $AppURI to $RootFolderApp\$AppDownloadFile"
-# try {
-#     $ProgressPreference = 'SilentlyContinue'
-#     Invoke-WebRequest -Uri $URI -OutFile "$RootFolderApp\$AppDownloadFile" -UseBasicParsing -ErrorAction Stop
-#     Write-Host "Downloaded $AppMoniker"
-# }
-# catch {
-#     Write-Host "Failed to download $AppMoniker"
-#     Exit 1
-# }
+Write-Host "Downloading $AppMoniker from $AppURI to $RootFolderApp\$AppDownloadFile"
+try {
+    $ProgressPreference = 'SilentlyContinue'
+    Invoke-WebRequest -Uri $URI -OutFile "$RootFolderApp\$AppDownloadFile" -UseBasicParsing -ErrorAction Stop
+    Write-Host "Downloaded $AppMoniker"
+}
+catch {
+    Write-Host "Failed to download $AppMoniker"
+    Exit 1
+}
 
 # Unzip content
 # Write-Host "Unzipping $AppDownloadFile"
@@ -61,14 +58,14 @@ Write-Host "WinGet path $Winget"
 # Expand-Archive -LiteralPath "$RootFolderApp\$AppDownloadFile" -DestinationPath $RootFolderAppUnzip -Force -Verbose
 
 # Do installation
-# Write-Host "Installing $AppMoniker"
-# $SetupFile = Join-Path -Path $RootFolderAppUnzip -ChildPath $AppSetupFile
-# $Result = Start-Process -FilePath $SetupFile -ArgumentList "/install /quiet /norestart" -Wait -Passthru
-# Write-Host "$AppMoniker installation result: $Result"
+ Write-Host "Installing $AppMoniker $SetupFile"
+ $SetupFile = Join-Path -Path $RootFolderAppUnzip -ChildPath $AppSetupFile
+ $Result = Start-Process -FilePath $SetupFile -ArgumentList "/install /quiet /norestart" -Wait -Passthru
+ Write-Host "$AppMoniker installation result: $Result"
 
 # Run WinGet Install
-Write-Host "Installing $AppMoniker - winget install --silent --scope machine --id `"$AppPublisher $AppName`"  --accept-source-agreements --accept-package-agreements --disable-interactivity" 
-&$winget install --silent --scope machine --id "$AppPublisher.$AppName"  --accept-source-agreements --accept-package-agreements --disable-interactivity
+# Write-Host "Installing $AppMoniker - winget install --silent --scope machine --id `"$AppPublisher $AppName`"  --accept-source-agreements --accept-package-agreements --disable-interactivity" 
+# &$winget install --silent --scope machine --id "$AppPublisher.$AppName"  --accept-source-agreements --accept-package-agreements --disable-interactivity
 
 Write-Host "###### $AppMoniker installation script is complete ######"
 exit 0
